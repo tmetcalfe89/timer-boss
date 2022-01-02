0e8a5a92cc6ff15b4c8f01bbefc0de2a1f50c80a
Tim <tmetcalfe89@gmail.com>
2022-01-02 09:10:52 -0800

    Fixed math

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useInterval, useAudio } from "react-use";
 
 function Timer({ hour, minute, start, name: outerName, removeTimer }) {
-  const [now, setNow] = useState(Date.now());
+  const [now, setNow] = useState(Math.floor(Date.now() / 1000) * 1000);
   const [audio, state, controls, ref] = useAudio({
     src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
     autoPlay: true,
@@ -10,21 +10,27 @@ function Timer({ hour, minute, start, name: outerName, removeTimer }) {
   const [name, setName] = useState(outerName);
 
   useInterval(() => {
-    setNow(Date.now());
+    setNow(Math.floor(Date.now() / 1000) * 1000);
   }, 100);
+
+  const getTargetTime = () =>
+    hour * 60 * 60 * 1000 + minute * 60 * 1000 + start;
+  const getElapsedTime = () => getTargetTime() - now;
+  const isTimeRemaining = () => now - start < getTargetTime();
+  const getRemainingHours = () => Math.floor(getElapsedTime() / 1000 / 60 / 60);
+  const getRemainingMinutes = () =>
+    Math.floor(getElapsedTime() / 1000 / 60) % 60;
+  const getRemainingSeconds = () => Math.floor(getElapsedTime() / 1000) % 60;
+  const getTimeDisplay = () =>
+    `${getRemainingHours()}:${getRemainingMinutes()
+      .toString()
+      .padStart(2, "0")}:${getRemainingSeconds().toString().padStart(2, "0")}`;
 
   return (
     <div>
-      {now - start < hour * 60 * 60 * 1000 + minute * 60 * 1000 ? (
+      {isTimeRemaining() ? (
         <>
-          <div>
-            {hour - Math.floor((now - start) / 1000 / 60 / 60)}:
-            {minute - (Math.floor((now - start) / 1000 / 60) % (60 * 60)) - 1}:
-            {`${60 - (Math.floor((now - start) / 1000) % 60) - 1}`.padStart(
-              2,
-              "0"
-            )}
-          </div>
+          <div>{getTimeDisplay()}</div>
           <div>
             <input value={name} onChange={(e) => setName(e.target.value)} />
           </div>
